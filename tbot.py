2d1933e6316675612fb24e6185378eb97c9940ba
Provonsal <alecsw86@gmail.com>
2023-04-29 02:22:42 +0700

    fixed hours

--- tbot.py
+++ tbot.py
@@ -1,10 +1,10 @@
 # coding=utf-8 
-from datetime import datetime
+from datetime import datetime, timedelta
 import telebot
 
 bot = telebot.TeleBot("6086891510:AAHhYBpEb_as4GwFW6Hw6N_y0yLcXDksW60")
 
-now = datetime.now() 
+now = datetime.now() + timedelta(hours=4)
 current_time = now.strftime("%H:%M:%S") 
 
 
