49d26d85c093d21c6dab5d661d763e0a5f7f2dbf
Provonsal <alecsw86@gmail.com>
2024-02-09 01:50:52 +0700

    asdasd

--- tbot.py
+++ tbot.py
@@ -2,7 +2,7 @@
 from datetime import datetime, timedelta
 import telebot
 
-bot = telebot.TeleBot("")
+bot = telebot.TeleBot("6269939624:AAGAv4FO_FD5JvfRlSSWPiednZXPesUbZhU")
 
 def current_time():
     now = datetime.now() + timedelta(hours=4)
